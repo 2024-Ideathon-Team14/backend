32243f7d3bf58c4e0bca6ea27885a177e897494c
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-05-31 23:58:45 +0900

    First commit

---
+++ src/test/java/tech/SimpleNews/SimpleNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.SimpleNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
