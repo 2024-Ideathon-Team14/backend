9bc622ef5a95b263af207e196eb603cf41177d41
donggyun <yje07052@gmail.com>
2024-06-01 05:13:54 +0900

    dummy
    dummy 추가

--- src/main/java/tech/SimpleNews/domain/UserApply.java
+++ src/main/java/tech/SimpleNews/domain/UserApply.java
@@ -1,39 +1,39 @@
-package tech.SimpleNews.domain;
+    package tech.SimpleNews.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.DynamicUpdate;
+    import jakarta.persistence.Column;
+    import jakarta.persistence.Entity;
+    import jakarta.persistence.FetchType;
+    import jakarta.persistence.GeneratedValue;
+    import jakarta.persistence.GenerationType;
+    import jakarta.persistence.Id;
+    import jakarta.persistence.JoinColumn;
+    import jakarta.persistence.ManyToOne;
+    import jakarta.persistence.Table;
+    import lombok.AllArgsConstructor;
+    import lombok.Builder;
+    import lombok.Getter;
+    import lombok.NoArgsConstructor;
+    import org.hibernate.annotations.DynamicUpdate;
 
-@Getter
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name = "user_apply")
-@Builder
-@DynamicUpdate
-public class UserApply {
+    @Getter
+    @Entity
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Table(name = "user_apply")
+    @Builder
+    @DynamicUpdate
+    public class UserApply {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_apply_id")
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "user_apply_id")
+        private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "user_id")
+        private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id")
-    private Post post;
-}
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "post_id")
+        private Post post;
+    }
